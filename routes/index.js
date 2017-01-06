33faf3f6b90f7c8e4de9ec21c0d1fce549cf364a
glnrzr <glnrzr@gmail.com>
2017-01-06 20:20:37 +0200

    Add files via upload

---
+++ routes/index.js
@@ -0,0 +1,16 @@
+
+/*
+ * GET home page.
+ */
+
+exports.index = function(req, res){
+  res.render('index', { title: 'Express' })
+};
+
+exports.chatPage = function(req, res){
+  res.render("chatPage", { layout: false });
+};
+
+exports.chatPage = function(req, res){
+  res.render("chatPage", { layout: false });
+};
